d5b649c77eeece9340b2c24517a87239d57020dd
tomohiro.yasukura <a0025575@yahoo.co.jp>
2020-06-10 10:33:26 +0000

    delete views/rename file...

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     belongs_to :user, optional: true
     
-    validates :content, presence: true, length: { maximum: 10 }
-    validates :status, presence: true, length: { maximum: 255 }
+    validates :content, presence: true, length: { maximum: 255 }
+    validates :status, presence: true, length: { maximum: 10 }
 end
